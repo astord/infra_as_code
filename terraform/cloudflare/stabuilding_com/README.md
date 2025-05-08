84857d343081a536cbbef306c5b3af8c69bde37f
Deyan Prodanov <deyan.prodanov@emerchantpay.com>
2025-05-08 23:45:25 +0300

    stabuilding domain terraform code

---
+++ terraform/cloudflare/stabuilding_com/README.md
@@ -0,0 +1,3 @@
+export TF_VAR_cloudflare_api_token=''
+export TF_VAR_account_id=''
+export TF_VAR_public_ip=''
---
+++ terraform/cloudflare/stabuilding_com/vars.tf
@@ -0,0 +1,12 @@
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "public_ip" {
+  type    = string
+  default = "46.10.240.219"
+}
