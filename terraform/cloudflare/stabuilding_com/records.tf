79cb58ccddd807e0d23a6a48d83e542954c1575f
Deyan Prodanov <deyan.prodanov@emerchantpay.com>
2025-05-14 23:25:44 +0300

    Stabuilding mails

--- terraform/cloudflare/stabuilding_com/records.tf
+++ terraform/cloudflare/stabuilding_com/records.tf
@@ -15,21 +15,37 @@ resource "cloudflare_dns_record" "cname_stabuilding" {
   type    = "A"
   ttl     = "600"
 }
-#resource "cloudflare_dns_record" "mx_10" {
-#  zone_id  = cloudflare_zone.monikanova_com.id
-#  content  = "mx1.loading.es."
-#  name     = "@"
-#  proxied  = false
-#  type     = "MX"
-#  priority = "10"
-#  ttl      = "600"
-#}
-#resource "cloudflare_dns_record" "mx_20" {
-#  zone_id  = cloudflare_zone.monikanova_com.id
-#  content  = "mx2.loading.es."
-#  name     = "@"
-#  proxied  = false
-#  type     = "MX"
-#  priority = "20"
-#  ttl      = "600"
-#}
+resource "cloudflare_dns_record" "cname_abv" {
+  zone_id = cloudflare_zone.stabuilding_com.id
+  content = "abv.bg"
+  name    = "abv-ab57fda4e34f03f09d1234cb4297.stabuilding.com"
+  proxied = false
+  type    = "CNAME"
+  ttl     = "60"
+}
+resource "cloudflare_dns_record" "mx_10" {
+  zone_id  = cloudflare_zone.stabuilding_com.id
+  content  = "appsmx1.abv.bg"
+  name     = "stabuilding.com"
+  proxied  = false
+  type     = "MX"
+  priority = "10"
+  ttl      = "600"
+}
+resource "cloudflare_dns_record" "mx_20" {
+  zone_id  = cloudflare_zone.stabuilding_com.id
+  content  = "appsmx2.abv.bg"
+  name     = "stabuilding.com"
+  proxied  = false
+  type     = "MX"
+  priority = "20"
+  ttl      = "600"
+}
+resource "cloudflare_dns_record" "txt_spf" {
+  zone_id = cloudflare_zone.stabuilding_com.id
+  content = "v=spf1 ip4:194.153.145.0/24 ~all"
+  name    = "@"
+  proxied = false
+  type    = "TXT"
+  ttl     = "60"
+}
