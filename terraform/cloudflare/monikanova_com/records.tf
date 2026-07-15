dda53b840b02e542c5e55756ff85cd48c7e6b5bf
Deyan Prodanov <deyan.prodanov@emerchantpay.com>
2026-07-15 23:05:57 +0300

    revert admin.monikanova.com redirect

--- terraform/cloudflare/monikanova_com/records.tf
+++ terraform/cloudflare/monikanova_com/records.tf
@@ -48,7 +48,7 @@ resource "cloudflare_dns_record" "a_ipv6" {
   ttl     = "600"
 }
 resource "cloudflare_dns_record" "cname_monikanova" {
-  for_each = toset(["lists", "autoconfig", "autodiscover", "www", "ftp", "admin"])
+  for_each = toset(["lists", "autoconfig", "autodiscover", "www", "ftp"])
 
   zone_id = cloudflare_zone.monikanova_com.id
   content = "monikanova.com"
